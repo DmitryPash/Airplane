60c4064cbc10e0466736677fb776e1cc64d6e60c
DmitryPashkou <86959216+DmitryPash@users.noreply.github.com>
2022-02-14 21:29:50 +0100

    com
    com

---
+++ dist/js/main.js
@@ -0,0 +1,65 @@
+//инициализация MFP popup для форм
+$(document).on("click", ".mfp-link", function () {
+  var a = $(this);
+  $.magnificPopup.open({
+    items: { src: a.attr("data-href") },
+    type: "ajax",
+    overflowY: "scroll",
+    removalDelay: 610,
+    mainClass: "my-mfp-zoom-in",
+    ajax: {
+      tError: "Error. Not valid url",
+    },
+    callbacks: {
+      open: function () {
+        setTimeout(function () {
+          $(".mfp-wrap, .mfp-bg").addClass("delay-back");
+          $(".mfp-popup").addClass("delay-back");
+        }, 700);
+      },
+    },
+  });
+  return false;
+});
+
+// Validation
+
+jQuery.validator.addMethod(
+  "lettersonly",
+  function (value, element) {
+    return this.optional(element) || /^[a-zA-ZА-Яа-я\s]+$/i.test(value);
+  },
+  "Incorrect format"
+);
+
+jQuery.validator.addMethod(
+  "telephone",
+  function (value, element) {
+    return (
+      this.optional(element) ||
+      /^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){6,14}(\s*)?$/i.test(value)
+    );
+  },
+  "Incorrect format"
+);
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
