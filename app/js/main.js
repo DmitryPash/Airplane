ed028348ca637a1c3565487bfc78a3f660904230
DmitryPashkou <86959216+DmitryPash@users.noreply.github.com>
2022-02-16 00:46:00 +0100

    con
    con

--- app/js/main.js
+++ app/js/main.js
@@ -43,14 +43,48 @@ jQuery.validator.addMethod(
   "Incorrect format"
 );
 
-const swiper = new Swiper(".swiper", {
-  // Optional parameters
-  loop: true,
+// const swiper = new Swiperzxc(".swiper", {
+//   // Optional parameters
+//   loop: true,
 
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
+//   // If we need pagination
+//   pagination: {
+//     el: ".swiper-pagination",
+//   },
+// });
+
+const swiperPodcasts = new Swiper(".podcasts-slider", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 100,
+    depth: 100,
+    scale: 0.85,
+    modifier: 1,
+    slideShadows: false,
   },
+  navigation: {
+    nextEl: ".podcasts-slider-next",
+    prevEl: ".podcasts-slider-prev",
+  },
+});
+swiperPodcasts.on("slideChange", function () {
+  $(".podcasts-slider iframe").remove();
+});
+$(document).on("click", ".podcasts-card-play", function (e) {
+  e.preventDefault();
+  const id = $(this).attr("data-video") || null;
+  const card = $(this).closest(".podcasts-card");
+
+  if (id) {
+    card.append(
+      `<iframe width="560" height="315" src="https://www.youtube.com/embed/${id}?autoplay=1" autoplay="1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+    );
+  }
 });
 
 // menu
